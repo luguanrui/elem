7b2b122a940d33abf9bb90c892839a13daf9de5e
luguanrui <1481744045@qq.com>
2017-11-20 13:37:39 +0800

    商品详情的样式布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,14 +2,39 @@
   <div class="goods">
     <div class="menu-wrapper">
       <ul>
-        <li v-for="(item,index) in goods" class="menu-item">
-          <span class="text">
+        <li v-for="(item,index) in goods" :key="index" class="menu-item">
+          <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="(item,index) in goods" :key="index" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="(food,index) in item.foods" :key="index" class="food-item border-1px">
+              <div class="icon">
+                <img :src="food.icon" alt="">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="description">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="nowPrice">￥{{food.price}}</span>
+                  <span v-show="food.oldPrice" class="oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -30,10 +55,10 @@
       this.$http.get('/api/goods').then(response=>{
         if(response.status === 200){
           this.goods = response.body;
-//          console.log(this.goods)
+          //  console.log(this.goods)
         }
       });
-      // 右侧menu的item的icon
+      // 左侧menu的item的icon
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
 
     }
@@ -41,9 +66,11 @@
 </script>
 
 <style lang="stylus">
+
   @import "../../common/stylus/mixin.styl"
+
   .goods
-    dispaly: flex
+    display: flex
     position: absolute
     top: 174px
     bottom: 46px
@@ -57,13 +84,19 @@
         display:table
         height: 54px
         width: 56px
+        padding:0 12px
         line-height: 14px
+        .text
+          display :table-cell
+          vertical-align:middle
+          font-size: 12px
+          border-1px(rgba(7,17,27,0.1))
         .icon
            display: inline-block
            vertical-align: top
            width: 12px
            height: 12px
-           margin-right: 4px
+           margin-right: 2px
            background-size: 12px 12px
            background-repeat: no-repeat
            &.decrease
@@ -78,4 +111,54 @@
                bg-image('special_3')
     .foods-wrapper
       flex:1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147,153,159)
+        background: #f3f5f7
+      .food-item
+        display:flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7,17,27,0.1))
+        &:lat-child
+          border-none()
+          margin-bottom:0
+        .icon
+          flex:0 0 57px
+          margin-right: 10px
+        .content
+          flex:1
+          margin-top: 2px
+          .name
+            height :14px
+            line-height: 14px
+            margin:2px 0 8px 0
+            color: rgb(7,17,27)
+            font-size: 14px
+          .description,.extra
+            line-height :10px
+            margin-bottom: 8px
+            font-size: 10px
+            color:rgb(147,153,159)
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            .nowPrice,.oldPrice
+              font-weight:700
+              line-height: 24px
+            .nowPrice
+              margin-right: 8px;
+              font-size: 14px
+              color:rgb(240,20,20)
+            .oldPrice
+              text-decoration:line-through
+              color:rgb(147,153,159)
+              font-size: 10px
+
+
 </style>
