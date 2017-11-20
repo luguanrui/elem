989c9e8d9fecc1422024edb3125f269c32814bcc
luguanrui <1481744045@qq.com>
2017-11-20 16:36:34 +0800

    better-scroll 实现menu和foods的滚动

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="(food,index) in item.foods" :key="index" class="food-item border-1px">
               <div class="icon">
-                <img :src="food.icon" alt="">
+                <img :src="food.icon" alt="" width="57" height="57">
               </div>
               <div class="content">
                 <h2 class="name">{{food.name}}</h2>
@@ -39,28 +39,40 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import Bscroll from 'better-scroll';
+
   export default {
-    props:{
-      seller:{
-        type:Object
+    props: {
+      seller: {
+        type: Object
       }
     },
     data(){
-      return{
-        goods:[]
+      return {
+        goods: []
       }
     },
     created(){
       // 请求数据
-      this.$http.get('/api/goods').then(response=>{
-        if(response.status === 200){
+      this.$http.get('/api/goods').then(response => {
+        if (response.status === 200) {
           this.goods = response.body;
+          this.$nextTick(()=>{
+            this._initScroll();
+          })
           //  console.log(this.goods)
         }
       });
       // 左侧menu的item的icon
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
-
+    },
+    methods:{
+       _initScroll(){
+         const menuWrapper = document.querySelector('.menu-wrapper');
+         const foodsWrapper = document.querySelector('.foods-wrapper');
+         this.menuScroll = new Bscroll(menuWrapper);
+         this.foodsWrapper = new Bscroll(foodsWrapper);
+      }
     }
   };
 </script>
@@ -79,85 +91,86 @@
     .menu-wrapper
       flex: 0 0 80px
       width: 80px
-      background:#f3f5f7
+      background: #f3f5f7
       .menu-item
-        display:table
+        display: table
         height: 54px
         width: 56px
-        padding:0 12px
+        padding: 0 12px
         line-height: 14px
         .text
-          display :table-cell
-          vertical-align:middle
+          display: table-cell
+          vertical-align: middle
           font-size: 12px
-          border-1px(rgba(7,17,27,0.1))
+          border-1px(rgba(7, 17, 27, 0.1))
         .icon
-           display: inline-block
-           vertical-align: top
-           width: 12px
-           height: 12px
-           margin-right: 2px
-           background-size: 12px 12px
-           background-repeat: no-repeat
-           &.decrease
-             bg-image('decrease_3')
-           &.discount
-             bg-image('discount_3')
-           &.guarantee
-             bg-image('guarantee_3')
-           &.invoice
-             bg-image('invoice_3')
-           &.special
-               bg-image('special_3')
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
     .foods-wrapper
-      flex:1
+      flex: 1
       .title
         padding-left: 14px
         height: 26px
         line-height: 26px
         border-left: 2px solid #d9dde1
         font-size: 12px
-        color: rgb(147,153,159)
+        color: rgb(147, 153, 159)
         background: #f3f5f7
       .food-item
-        display:flex
+        display: flex
         margin: 18px
         padding-bottom: 18px
-        border-1px(rgba(7,17,27,0.1))
+        border-1px(rgba(7, 17, 27, 0.1))
         &:lat-child
           border-none()
-          margin-bottom:0
+          margin-bottom: 0
         .icon
-          flex:0 0 57px
+          flex: 0 0 57px
           margin-right: 10px
         .content
-          flex:1
-          margin-top: 2px
+          flex: 1
           .name
-            height :14px
+            height: 14px
             line-height: 14px
-            margin:2px 0 8px 0
-            color: rgb(7,17,27)
+            margin: 2px 0 8px 0
+            color: rgb(7, 17, 27)
             font-size: 14px
-          .description,.extra
-            line-height :10px
-            margin-bottom: 8px
+          .description, .extra
+            line-height: 10px
             font-size: 10px
-            color:rgb(147,153,159)
+            color: rgb(147, 153, 159)
+          .description
+            margin-bottom: 8px
+            line-height: 14px
           .extra
             .count
               margin-right: 12px
           .price
-            .nowPrice,.oldPrice
-              font-weight:700
+            .nowPrice, .oldPrice
+              font-weight: 700
               line-height: 24px
             .nowPrice
               margin-right: 8px;
               font-size: 14px
-              color:rgb(240,20,20)
+              color: rgb(240, 20, 20)
             .oldPrice
-              text-decoration:line-through
-              color:rgb(147,153,159)
+              text-decoration: line-through
+              color: rgb(147, 153, 159)
               font-size: 10px
 
 
