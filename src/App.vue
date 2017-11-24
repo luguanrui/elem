550ae49c91d2563a930b40bf6c6878a2366737d4
luguanrui <luguanrui@github.cm>
2017-11-24 16:30:59 +0800

    父组件App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
+  <!--最外层APP组件-->
   <div class="app">
     <div class="header">
+      <!--header组件，通过props传入数据，seller对象-->
       <v-header v-bind:seller="seller"></v-header>
     </div>
     <div class="tab">
@@ -14,29 +16,35 @@
         <router-link to="/seller">商家</router-link>
       </div>
     </div>
+    <!--路由-->
     <router-view :seller="seller"></router-view>
   </div>
 </template>
 
 <script>
+  //  引入header组件
   import header from 'components/header/header.vue'
 
   export default {
-    name: 'app',
-    data(){
-      return{
-          seller:{}
+    data() {
+      return {
+        // 初始化变量seller
+        seller: {}
       }
     },
-    created(){
-        this.$http.get('/api/seller').then((response)=>{
-          if(response.status === 200){
-              this.seller = response.body;
+    created() {
+      //  获取数据vue-resource
+      this.$http.get('/api/seller').then((response) => {
+//        console.log(response);
+        if (response.status === 200) {
+          //  将获取到的数据赋值给变量seller
+          this.seller = response.body;
 //              console.log(this.seller);
-          }
+        }
 
-        })
+      })
     },
+    // 注册组件
     components: {
       'v-header': header
     }
